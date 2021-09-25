4b582c72406b35630e2a43dead9696dce788890c
Takashi <th2826@columbia.edu>
2021-09-25 13:47:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord
 }
 
-/* 
-  Add your pseudocode here
+function isPalindrome(word) {
+  // reverse the input string by calling reverseString() and assign the return value to a variable reverseWord
+  const reversedWord = reverseString(word);
+  // test if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+/* Add your pseudocode here
+  reverse the input string
+
+  if the reversed string is the same as the input
+    return true
+  else
+    return false  
 */
 
 /*
-  Add written explanation of your solution here
+  create an array from the input string
+  reverse the array
+  create a string from the reversed array
+  return the reversed string
 */
 
 // You can run `node index.js` to view these console logs
